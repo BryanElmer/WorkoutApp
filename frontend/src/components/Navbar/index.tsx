1f2f523a5d1fbdb281b69ea1e7f971886a06eec1
Bryan Elmer <bryan.elmer.mulijono@gmail.com>
2024-06-18 18:07:05 +0800

    Frontend: update navbar buttons for user logouts

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,7 +1,17 @@
+"use client";
+
 import Link from "next/link"
 import styles from "./index.module.css"
+import { useLogout } from "@/hooks/useLogout"
+import { useAuthContext } from "@/hooks/useAuthContext";
 
 const Navbar = () => {
+  const { logout } = useLogout()
+  const { state } = useAuthContext()
+
+  const handleClick = () => {
+    logout()
+  }
 
   return (
     <header className={styles.navbar}>
@@ -10,10 +20,18 @@ const Navbar = () => {
           <h1 className="font-bold">Workout Buddy</h1>
         </Link>
         <nav className={styles.nav}>
-          <div>
-            <Link href={"/login"}>Login</Link>
-            <Link href={"/signup"}>Signup</Link>
-          </div>
+          {state.user && (
+            <div>
+              <span>{state.user?.email}</span>
+              <button onClick={handleClick}>Log out</button>
+            </div>
+          )}
+          {!state.user && (
+            <div>
+              <Link href={"/login"}>Login</Link>
+              <Link href={"/signup"}>Signup</Link>
+            </div>
+          )}
         </nav>
       </div>
     </header>
